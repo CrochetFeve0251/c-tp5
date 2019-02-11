aa64a9b6bd2726098cffd557e5e99bc6df7ce7ed
COQUARD Cyrille <crochetfeve0251@gmail.com>
2019-02-11 10:00:28 +0100

    Added test function pokemon

--- main.cpp
+++ main.cpp
@@ -2,11 +2,16 @@
 // Created by cf0 on 11/02/19.
 //
 #include <iostream>
+#include "Pokemon.h"
 
 void testPokemon(){
-
+    Pokemon p1("test", "test");
+    std::cout << p1 << std::endl;
+    std::cin >> p1;
+    std::cout << p1 << std::endl;
 }
 
 int main() {
+    testPokemon();
     return 0;
 }
