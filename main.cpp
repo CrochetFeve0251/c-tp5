38ed665b5a4d78defc04f5f0d64669832d2ba1d6
COQUARD Cyrille <crochetfeve0251@gmail.com>
2019-02-11 10:19:48 +0100

    Added sort and search pika

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 // Created by cf0 on 11/02/19.
 //
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include "Pokemon.h"
 
 void testPokemon(){
@@ -9,6 +11,18 @@ void testPokemon(){
     std::cout << p1 << std::endl;
     std::cin >> p1;
     std::cout << p1 << std::endl;
+}
+
+void searchPikachu(std::vector v) {
+    for(Pokemon p : v)
+        if(p.name == "Pikachu")
+            std::cout << p << std::endl;
+}
+
+void sortPokemons(std::vector v){
+    std::sort(v.begin(),v.end());
+    for(Pokemon p : v)
+        std::cout << p << std::endl;
 }
 
 int main() {
