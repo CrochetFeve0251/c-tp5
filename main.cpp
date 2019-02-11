a333d1361af8be93e2940e5fb8062b8b0b818849
root <root@localhost.localdomain>
2019-02-11 03:14:28 -0600

    commit mahdi

--- main.cpp
+++ main.cpp
@@ -4,5 +4,19 @@
 #include <iostream>
 
 int main() {
-    return 0;
-}
+
+ ifstream ifs("Pokemons.txt");
+ if (ifs) {
+ Pokemon p;
+ while (!ifs.eof()) {
+ ifs >> p;
+ if (!ifs.fail()) {
+ cout << p;
+ }
+ }
+ ifs.close();
+ }
+ else {
+ cerr << "Impossible d'ouvrir le fichier " << endl;
+ }
+ }
